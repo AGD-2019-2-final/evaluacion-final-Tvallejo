5395aaa743778f4a8a4586429418114d136b4ff4
Tvallejo <30671819+Tvallejo@users.noreply.github.com>
2020-02-13 00:49:38 -0500

    quiz 4

--- 01-hadoop-50/q04-10/reducer.py
+++ 01-hadoop-50/q04-10/reducer.py
@@ -2,4 +2,17 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
-
+if __name__ == "__main__":
+    clave_actual = None
+    total = 0
+    for line in sys.stdin:
+        clave, valor = line.split("\t")
+        valor = int(valor)
+        if clave_actual == clave:
+            total += valor
+        else:
+            if clave_actual is not None:
+                print("{},{}".format(clave_actual, total))
+            clave_actual = clave
+            total = valor
+    print("{},{}".format(clave_actual, total))
