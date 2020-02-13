9892696aad74903127ac020dd1b4343b89b23500
Tvallejo <30671819+Tvallejo@users.noreply.github.com>
2020-02-13 01:56:27 -0500

    quiz 10

--- 01-hadoop-50/q10-10/reducer.py
+++ 01-hadoop-50/q10-10/reducer.py
@@ -2,3 +2,17 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    clave_actual = None
+    numero_actual = ""
+    for line in sys.stdin:
+        tmp, clave, valor = line.split("\t")
+        valor = valor.strip()
+        if clave_actual == clave:
+            numero_actual = "{},{}".format(numero_actual, valor)
+        else:
+            if clave_actual is not None:
+                print("{}\t{}".format(clave_actual, numero_actual))
+            clave_actual = clave
+            numero_actual = valor
+    print("{}\t{}".format(clave_actual, numero_actual))
