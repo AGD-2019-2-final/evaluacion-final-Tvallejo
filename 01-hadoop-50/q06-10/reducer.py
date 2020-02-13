879cf113a3edc0ab6aba85895ad282569022bc38
Tvallejo <30671819+Tvallejo@users.noreply.github.com>
2020-02-13 01:20:08 -0500

    quiz 06

--- 01-hadoop-50/q06-10/reducer.py
+++ 01-hadoop-50/q06-10/reducer.py
@@ -2,3 +2,19 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    clave_actual = None
+    minimo = 0; maximo = 0
+    for line in sys.stdin:
+        clave, valor = line.split("\t")
+        valor = float(valor)
+        if clave_actual == clave:
+            minimo = valor if valor < minimo else minimo
+            maximo = valor if valor > maximo else maximo
+        else:
+            if clave_actual is not None:
+                print("{}\t{}\t{}".format(clave_actual, maximo, minimo))
+            clave_actual = clave
+            minimo = valor
+            maximo = valor
+    print("{}\t{}\t{}".format(clave_actual, maximo, minimo))
